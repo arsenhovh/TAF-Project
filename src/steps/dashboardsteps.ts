a964e8e01bac5602f03f89d94babb18e832e32e1
Arsen Hovhannisyan <arsen_hovhannisyan@epam.com>
2024-05-13 23:30:21 +0400

    minor update

--- src/steps/dashboardsteps.ts
+++ src/steps/dashboardsteps.ts
@@ -1,4 +1,4 @@
-import { Given, Then, Before,After } from '@cucumber/cucumber';
+import { Given, Then, Before,After,When } from '@cucumber/cucumber';
 import { Page, chromium } from 'playwright';
 import { BasePage } from "../../core/pageObject/Base";
 import {expect} from "@playwright/test";
@@ -29,39 +29,35 @@ Given('I navigate on the login page and login', async () => {
     await loginPage.loginAsTestUser();
 });
 
-Given('I click add new dashboard page', async () => {
+When('I click add new dashboard page', async () => {
     await dashboardPage.addNewDashboard();
 });
 
-Given(/^I fill (.*) and (.*) in name and description field$/, async (dashboardName, description) => {
+When(/^I fill (.*) and (.*) in name and description field$/, async (dashboardName, description) => {
     await dashboardPage.newDashboardNameFromPopup.fill(dashboardName);
     await dashboardPage.description.fill(description);
 });
 
-Given('I click add save button', async () => {
+When('I click add save button', async () => {
         await dashboardPage.addDashboardButtonFromPopup.click();
 });
 
-Given('I click add edit button', async () => {
+When('I click add edit button', async () => {
     await dashboardPage.editDashboardButton.click();
 });
 
-Given(/^I expect to see newly added dashboard with name (.*)$/, async (dashboardName) => {
+Then(/^I expect to see newly added dashboard with name (.*)$/, async (dashboardName) => {
     await expect.soft(dashboardPage.dashboardAddedSuccessfully).toBeVisible();
     await expect.soft(dashboardPage.dashboardNameFromMenu).toHaveText(dashboardName);
 });
 
-Given(/^I delete dashboard$/, async () => {
+When(/^I delete dashboard$/, async () => {
     await dashboardPage.deleteDashboardButton.click();
     await dashboardPage.confirmDeleteButton.click();
 });
 
-Given(/^I expect to not dashboard$/, async () => {
+Then(/^I expect to not dashboard$/, async () => {
     await expect.soft(dashboardPage.dashboardDeletedSuccessfully).toBeVisible();
     await expect.soft(dashboardPage.dashboardNameFromMenu).not.toBeVisible();
-});
-
-Given(/^I delete all dashboard$/, async () => {
-
 });
 
